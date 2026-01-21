89c6d1e513f621be77524dd2c1dafdcf3ff72068
Al-aqua <mohammad@alaqua.dev>
2026-01-21 23:13:46 +0300

    Add navbar and theme toggle

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import ThemeToggle from './theme_toggle.svelte';
+</script>
+
+<nav class="fixed h-16 w-full">
+	<div class="flex h-full w-full items-center justify-around">
+		<div></div>
+		<div></div>
+		<div>
+			<ThemeToggle />
+		</div>
+	</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Hero from '$lib/components/hero.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
 </script>
 
 <svelte:head>
 	<title>Mohammad Al-aqua</title>
 </svelte:head>
 
+<Navbar />
 <Hero />

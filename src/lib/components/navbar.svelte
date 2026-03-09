ee3fa6198c57f876c074475bbf18d746133ca5cf
Al-aqua <mohammad@alaqua.dev>
2026-03-10 02:32:46 +0300

    Make navbar responsive

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,29 +1,43 @@
 <script lang="ts">
 	import ThemeToggle from './theme_toggle.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { Terminal } from '@lucide/svelte';
+	import { Terminal, Menu, X } from '@lucide/svelte';
+	import { slide } from 'svelte/transition';
 	import type { NavItem } from '$lib/types';
 	interface props {
 		links: NavItem[];
 	}
 	let { links }: props = $props();
+
+	let isMenuOpen = $state(false);
 </script>
 
-<nav class="fixed z-50 h-16 w-full bg-background">
-	<div class="flex h-full w-full items-center justify-around">
+<nav class="fixed z-50 h-16 w-full border-b bg-background">
+	<div class="flex h-full w-full items-center justify-between p-4 md:justify-around">
 		<ul class="flex gap-8 md:gap-28">
-			<li>
-				<Button
-					href="/"
-					class="text-md font-family-heading transition-all hover:text-foreground"
-					variant="outline"
-					role="link"
-					aria-label="Home - Al-aqua portfolio"
-				>
-					<Terminal /> Al-aqua
-				</Button>
-			</li>
-			<div class="flex gap-2">
+			<div class="flex items-center gap-2">
+				<li class="md:hidden">
+					<Button variant="ghost" size="icon" onclick={() => (isMenuOpen = !isMenuOpen)}>
+						{#if isMenuOpen}
+							<X />
+						{:else}
+							<Menu />
+						{/if}
+					</Button>
+				</li>
+				<li>
+					<Button
+						href="/"
+						class="text-md font-family-heading transition-all hover:text-foreground"
+						variant="outline"
+						role="link"
+						aria-label="Home - Al-aqua portfolio"
+					>
+						<Terminal /> Al-aqua
+					</Button>
+				</li>
+			</div>
+			<div class="hidden gap-2 md:flex">
 				{#each links as link (link.name)}
 					<li>
 						<Button
@@ -39,9 +53,24 @@
 				{/each}
 			</div>
 		</ul>
-		<div></div>
 		<div>
 			<ThemeToggle />
 		</div>
 	</div>
+
+	{#if isMenuOpen}
+		<div transition:slide class="flex flex-col gap-2 border-b bg-background p-4 md:hidden">
+			{#each links as link (link.name)}
+				<Button
+					href={link.href}
+					variant="ghost"
+					class="w-fit justify-start"
+					role="link"
+					onclick={() => (isMenuOpen = false)}
+				>
+					{link.name}
+				</Button>
+			{/each}
+		</div>
+	{/if}
 </nav>
